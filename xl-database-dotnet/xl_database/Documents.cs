015ac09e91d1b4c69d6f250881dc917f25f8046f
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-03-28 11:09:56 +0100

    WIP

--- xl-database-dotnet/xl_database/Documents.cs
+++ xl-database-dotnet/xl_database/Documents.cs
@@ -9,11 +9,11 @@ public class AnalysisResult
     public string Id { get; set; }
 
     /// <summary>
-    /// The identifier for the sample that was analyzed. This could be
-    /// a unique sample ID, a batch number, or any other identifier that
-    /// links the analysis result to a specific sample
+    /// The date and time when the analysis was performed. This should
+    /// be stored in a standardized format (e.g., ISO 8601) to ensure
+    /// consistency across different systems and time zones.
     /// </summary>
-    public string SampleId { get; set; }
+    public DateTime Date { get; set; }
 
     /// <summary>
     /// The type of analysis performed. See <see cref="AnalysisType"/>.
@@ -26,11 +26,11 @@ public class AnalysisResult
     public string MachineId { get; set; }
 
     /// <summary>
-    /// The date and time when the analysis was performed. This should
-    /// be stored in a standardized format (e.g., ISO 8601) to ensure
-    /// consistency across different systems and time zones.
+    /// The identifier for the sample that was analyzed. This could be
+    /// a unique sample ID, a batch number, or any other identifier that
+    /// links the analysis result to a specific sample
     /// </summary>
-    public DateTime Date { get; set; }
+    public string SampleId { get; set; }
 
     /// <summary>
     /// The actual data from the analysis. TBD based on actual types.
@@ -38,18 +38,18 @@ public class AnalysisResult
     public string Data { get; set; }
 
     public AnalysisResult(
-        string sampleId,
         AnalysisType type,
         string machineId,
+        string sampleId,
         string data
         )
     {
         Id   = Guid.NewGuid().ToString();
         Date = DateTime.UtcNow;
 
-        SampleId  = sampleId;
         Type      = type;
         MachineId = machineId;
+        SampleId  = sampleId;
         Data      = data;
     }
 }
